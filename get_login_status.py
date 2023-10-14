e706f29c3746e0b4764e33e5f6eabe144089e508
jyx-su <108294040+jyx-su@users.noreply.github.com>
2023-10-13 21:38:56 -0700

    Add files via upload

---
+++ get_login_status.py
@@ -0,0 +1,17 @@
+import asyncio
+from playwright.async_api import async_playwright, Playwright
+import time
+
+async def run(playwright: Playwright):
+    firefox = playwright.firefox
+    browser = await firefox.launch_persistent_context('', headless=False, timeout=0)
+    page = await browser.new_page()
+    await page.goto("https://chat.openai.com")
+    time.sleep(60)
+    storage = await browser.storage_state(path="state.json")
+    
+async def main():
+    async with async_playwright() as playwright:
+        await run(playwright)
+
+asyncio.run(main())
